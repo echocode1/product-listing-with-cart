7032f0442d9246eb69f3d0fa7b02b5c5ffa51e88
echo123 <julietoscar0000gmail.com>
2024-10-14 01:26:24 +0100

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,8 @@ export const metadata: Metadata = {
   title: "Product list with cart",
   description: "Cart list that adds, removes, and makes orders as desired",
   icons: {
-    icon: "/src/ui/public/assets/images/favicon-32x32.png",
+    icon: "/assets/images/favicon-32x32.png",
   },
-};
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
 };
 
 export default function RootLayout({
@@ -29,17 +24,16 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-            <body>
-              <main> 
-                <CartProvider>
-                  {children}
-                </CartProvider>    
-              </main>
-              
-            </body>
-      </ThemeProvider>
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <main>
+            <CartProvider>
+              {children}
+            </CartProvider>
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
